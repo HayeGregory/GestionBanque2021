35190e852d5ae0281e3fe435bde252e6ccde80fb
HayeGregory <haye.greg@outlook.com>
2021-03-13 20:13:39 +0100

    Step 1 [EX_p63] Personne Courant

--- GestionBanque2021/Program.cs
+++ GestionBanque2021/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Models;
+using System;
 
 namespace GestionBanque2021
 {
@@ -6,7 +7,60 @@ namespace GestionBanque2021
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Gestion bancaire\n----------------");
+
+            Personne ClientA = new Personne()
+            {
+                Nom = "Haye",
+                Prenom = "Gregory",
+                DateNaiss = new DateTime(1979, 06, 14)
+            };
+            Personne ClientB = new Personne()
+            {
+                Nom = "Hubert",
+                Prenom = "Emmanuelle",
+                DateNaiss = new DateTime(1975, 10, 25)
+            };
+
+            Console.WriteLine($"Client A: {ClientA.Nom} {ClientA.Prenom} | {ClientA.DateNaiss } ");
+            Console.WriteLine($"Client B: {ClientB.Nom} {ClientB.Prenom} | {ClientB.DateNaiss } ");
+
+            Courant C1CA = new Courant
+            {
+                Numero = "1111",
+                LigneDeCredit = 1000,
+                Titulaire = ClientA
+            };
+            Courant C1CB = new Courant
+            {
+                Numero = "3333",
+                LigneDeCredit = 1000,
+                Titulaire = ClientB
+            };
+            Courant C2CA = new Courant
+            {
+                Numero = "2222",
+                LigneDeCredit = 0,
+                Titulaire = ClientA
+            };
+
+            AffichageComptes(C1CA, C1CB, C2CA);
+            C1CA.Depot(1000);
+            C1CB.Depot(2000);
+            C2CA.Depot(500);
+            C1CA.Retrait(2000);
+            AffichageComptes(C1CA, C1CB, C2CA);
+        }
+
+        private static void AffichageComptes(Courant C1CA, Courant C1CB, Courant C2CA)
+        {
+            Console.WriteLine("------------------------------------------------------------------------------");
+            Console.WriteLine($"C1CA : {C1CA.Numero} | solde : {C1CA.Solde,2} | LDC : {C1CA.LigneDeCredit,2} | " +
+                                $"titulaire : {C1CA.Titulaire.Prenom} {C1CA.Titulaire.Prenom}");
+            Console.WriteLine($"C2CA : {C2CA.Numero} | solde : {C2CA.Solde,2} | LDC : {C2CA.LigneDeCredit,2} | " +
+                                $"titulaire : {C2CA.Titulaire.Prenom} {C2CA.Titulaire.Prenom}");
+            Console.WriteLine($"C1CB : {C1CB.Numero} | solde : {C1CB.Solde,2} | LDC : {C1CB.LigneDeCredit,2} | " +
+                                $"titulaire : {C1CB.Titulaire.Prenom} {C1CB.Titulaire.Prenom}");
         }
     }
 }
---
+++ Models/Courant.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class Courant
+    {
+        // attributes
+        private string _numero;
+        private double _solde;
+        private double _ligneDeCredit;
+        private Personne _titulaire;
+
+        // props
+        public Personne Titulaire
+        {
+            get { return _titulaire; }
+            set { _titulaire = value; }
+        }
+
+        public double LigneDeCredit
+        {
+            get { return _ligneDeCredit; }
+            set
+            {
+                if (value > 0)
+                {
+                    _ligneDeCredit = value;
+                }
+            }
+        }
+        public double Solde
+        {
+            get { return _solde; }
+            private set { _solde = value; }
+        }
+
+        public string Numero
+        {
+            get { return _numero; }
+            set { _numero = value; }
+        }
+
+        // methods
+        public void Retrait(double montant)
+        {
+            if (montant > 0)
+            {
+                if (Solde + LigneDeCredit >= montant)
+                {
+                    Solde -= montant;
+                }
+                else
+                {
+                    // exception : solde insuffisant
+                }
+            }
+            else
+            {
+                // exception montant negatif
+            }
+        }
+        public void Depot(double montant)
+        {
+            if (montant > 0)
+            {
+                Solde += montant;
+            }
+            else
+            {
+                // exception montant negatif
+            }
+        }
+    }
+}
